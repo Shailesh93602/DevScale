5f2eedbfcb3307a1e5f0caaff3ece4ab3256823c
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-05-01 10:35:00 -0700

    added: middlewares for protecting routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,16 @@
 import { config } from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
-import authRoutes from './src/routes/authRoutes.js';
-import userRoutes from './src/routes/userRoutes.js';
+import routes from './src/routes/routes.js';
 
 config();
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
 
 const port = process.env.PORT || 3000;
-const url = process.env.DB_URL;
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
 
 app.listen(port, () => {
     console.log("Server is listing on port: " + port);
