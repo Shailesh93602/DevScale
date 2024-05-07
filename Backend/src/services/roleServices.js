c5d97f8ec0d4606a37720da4a2803b148e8ad7ef
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-05-06 22:04:37 -0700

    added: file upload middler with multer to upload files such as profile photo

---
+++ Backend/src/services/roleServices.js
@@ -0,0 +1,12 @@
+export const validateRole = (role_id) => (req, res, next) => {
+    if (!req.user) {
+      return res.redirect("/u/signIn");
+    }
+    if (req.user.role_id == role_id) {
+      next();
+    } else {
+      res.redirect("/u/signIn");
+    }
+  }
+  
+  
