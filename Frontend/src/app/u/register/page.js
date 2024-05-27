0d3e1965f344ea02fe5db26236fa74cc7b35557a
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-05-27 18:51:01 +0530

    auth commit

--- Frontend/src/app/u/register/page.js
+++ Frontend/src/app/u/register/page.js
@@ -2,80 +2,183 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import { Button, ButtonGroup } from "@nextui-org/button";
+import { useNavigate } from "react-router-dom";
+import { useRouter } from "next/navigation";
 
 export default function Register() {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [repeatPassword, setRepeatPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!username) return document.getElementById('username').focus();
-        if (!email) return document.getElementById('email').focus();
-        if (!password) return document.getElementById('password').focus();
-        if (repeatPassword !== password) return document.getElementById('repeatPassword').focus();
-        let terms = document.getElementById('terms');
-        if (!terms.checked) return terms.focus();
+  const router = useRouter();
 
-        const result = await fetch("http://localhost:4000/auth/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            })
-        });
-        const json = await result.json();
-        if (json.success) console.log(json.message);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!username) return document.getElementById("username").focus();
+    if (!email) return document.getElementById("email").focus();
+    if (!password) return document.getElementById("password").focus();
+    if (repeatPassword !== password)
+      return document.getElementById("repeatPassword").focus();
+    let terms = document.getElementById("terms");
+    if (!terms.checked) return terms.focus();
+
+    console.log(username, email, password);
+
+    try {
+      const result = await fetch("http://localhost:4000/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+      });
+      const json = await result.json();
+
+      if (json.success) {
+        console.log(json.message);
+        router.push("/");
+      } else {
+        console.log(json.message);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    return (
-        <section className="bg-gray-50">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <Link href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-                    Mr. Engineers
-                </Link>
-                <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                            Create an account
-                        </h1>
-                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-                            <div>
-                                <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">Your username</label>
-                                <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="john1998" required="" value={username} onChange={(e) => setUsername(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" value={password} onChange={(e) => setPassword(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="repeatPassword" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
-                                <input type="password" name="repeatPassword" id="repeatPassword" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} />
-                            </div>
-                            <div className="flex items-start">
-                                <div className="flex items-center h-5">
-                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="" />
-                                </div>
-                                <div className="ml-3 text-sm">
-                                    <label htmlFor="terms" className="font-light text-gray-500">I accept the <Link className="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</Link></label>
-                                </div>
-                            </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
-                            <p className="text-sm font-light text-gray-500">
-                                Already have an account? <a href="/u/login" className="font-medium text-primary-600 hover:underline">Login here</a>
-                            </p>
-                        </form>
-                    </div>
+  };
+  return (
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Link
+          href="/"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+        >
+          Mr. Engineers
+        </Link>
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Create an account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Your username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="john1998"
+                  required=""
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="name@company.com"
+                  required=""
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="repeatPassword"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Confirm password
+                </label>
+                <input
+                  type="password"
+                  name="repeatPassword"
+                  id="repeatPassword"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                  value={repeatPassword}
+                  onChange={(e) => setRepeatPassword(e.target.value)}
+                />
+              </div>
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="terms"
+                    aria-describedby="terms"
+                    type="checkbox"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                    required=""
+                  />
                 </div>
-            </div>
-        </section>
-    );
+                <div className="ml-3 text-sm">
+                  <label htmlFor="terms" className="font-light text-gray-500">
+                    I accept the{" "}
+                    <Link
+                      className="font-medium text-primary-600 hover:underline"
+                      href="#"
+                    >
+                      Terms and Conditions
+                    </Link>
+                  </label>
+                </div>
+              </div>
+              {/* <Button color="red ">Button</Button> */}
+              <button
+                type="submit"
+                className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Create an account
+              </button>
+              <p className="text-sm font-light text-gray-500">
+                Already have an account?{" "}
+                <a
+                  href="/u/login"
+                  className="font-medium text-primary-600 hover:underline"
+                >
+                  Login here
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
