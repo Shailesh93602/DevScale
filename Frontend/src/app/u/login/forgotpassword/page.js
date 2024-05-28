4f413b15d67d71d99cbcd50f15b20db7bdfea5e7
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-05-28 14:15:26 +0530

    completed auth functionality

---
+++ Frontend/src/app/u/login/forgotpassword/page.js
@@ -0,0 +1,74 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+export default function forgotPassword() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!email) return document.getElementById("username").focus();
+
+    let result = await fetch("http://localhost:4000/auth/forgotPassword", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+      }),
+      credentials: "include",
+    });
+    console.log(result);
+    let json = await result.json();
+    if (json.success) console.log(json);
+  };
+  return (
+    <section className="bg-gray-50 ">
+      <div className="flex mb-60 flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Link
+          href="/"
+          className="flex items-center mb-4 text-2xl font-semibold text-gray-900"
+        >
+          Mr. Engineers
+        </Link>
+        <div className="w-full  bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6  space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl flex justify-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              forgot password
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  {" "}
+                  Enter yout Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="john@example.com"
+                  required=""
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+              >
+                submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
