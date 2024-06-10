ce46e9bdf8dbc6c7ab08ae96688396b633aa8437
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-06-10 08:18:00 +0530

    added: monaco editor for running code

---
+++ Frontend/src/components/Editor.jsx
@@ -0,0 +1,27 @@
+"use client";
+import dynamic from "next/dynamic";
+
+const MonacoEditor = dynamic(import("@monaco-editor/react"), { ssr: false });
+
+const EditorComponent = ({ value, onChange }) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <MonacoEditor
+        height="100%"
+        language="javascript"
+        theme="vs-dark"
+        value={value}
+        onChange={onChange}
+        options={{
+          selectOnLineNumbers: true,
+          roundedSelection: false,
+          readOnly: false,
+          cursorStyle: "line",
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default EditorComponent;
