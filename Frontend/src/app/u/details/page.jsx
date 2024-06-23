1642c90a8dc3a98dc9f5391508a6ffc26cc9e528
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-06-23 18:36:52 +0530

    Save point

--- Frontend/src/app/u/details/page.jsx
+++ Frontend/src/app/u/details/page.jsx
@@ -1,13 +1,5 @@
 "use client";
-
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { toast } from "react-toastify";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import { useState } from "react";
 import {
   Form,
   FormControl,
@@ -16,7 +8,10 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -25,37 +20,17 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-const formSchema = [
-  yup.object().shape({
-    fullName: yup.string().required("Full Name is required."),
-    dob: yup.date().required("Date of Birth is required."),
-    gender: yup.string().required("Gender is required."),
-  }),
-  yup.object().shape({
-    mobile: yup.string().required("Mobile Number is required."),
-    whatsapp: yup.string(),
-    address: yup.string().required("Address is required."),
-  }),
-  yup.object().shape({
-    university: yup.string().required("University Name is required."),
-    college: yup.string().required("College Name is required."),
-    branch: yup.string().required("Branch Name is required"),
-    semester: yup
-      .number("Semester must be a number")
-      .required("Semester Name is required"),
-  }),
-];
-
-const Step1 = ({ control, errors }) => (
-  <div>
+const StepOne = ({ control }) => (
+  <>
+    <h2 className="text-2xl font-semibold">Step 1: Basic Information</h2>
     <FormField
       control={control}
-      name="fullName"
+      name="name"
       render={({ field }) => (
         <FormItem>
-          <FormLabel>Full Name</FormLabel>
+          <FormLabel>Name</FormLabel>
           <FormControl>
-            <Input placeholder="Enter your Full Name" {...field} />
+            <Input placeholder="Enter your Name" {...field} />
           </FormControl>
           <FormMessage />
         </FormItem>
@@ -63,122 +38,23 @@ const Step1 = ({ control, errors }) => (
     />
     <FormField
       control={control}
-      name="dob"
+      name="age"
       render={({ field }) => (
         <FormItem>
-          <FormLabel>Date of Birth</FormLabel>
+          <FormLabel>Age</FormLabel>
           <FormControl>
-            <Input
-              {...field}
-              type="date"
-              id="dob"
-              className={`border ${
-                errors?.dob ? "border-red-500" : "border-gray-300"
-              } rounded-md px-3 py-2`}
-            />
+            <Input placeholder="Enter your Age" {...field} />
           </FormControl>
           <FormMessage />
         </FormItem>
       )}
     />
-    <FormField
-      control={control}
-      name="gender"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>Gender</FormLabel>
-          <Select onValueChange={field.onChange} defaultValue={field.value}>
-            <FormControl>
-              <SelectTrigger>
-                <SelectValue placeholder="Select you gender" />
-              </SelectTrigger>
-            </FormControl>
-            <SelectContent>
-              <SelectItem value="male">Male</SelectItem>
-              <SelectItem value="female">Female</SelectItem>
-              <SelectItem value="other">Other</SelectItem>
-            </SelectContent>
-          </Select>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-  </div>
+  </>
 );
 
-const Step2 = ({ control }) => (
-  <div>
-    <FormField
-      control={control}
-      name="mobile"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>Mobile Number</FormLabel>
-          <FormControl>
-            <Input placeholder="Enter your Phone Number" {...field} />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-    <FormField
-      control={control}
-      name="whatsapp"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>WhatsApp Number</FormLabel>
-          <FormControl>
-            <Input placeholder="Enter your Whatsapp Number" {...field} />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-
-    <FormField
-      control={control}
-      name="address"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>Address</FormLabel>
-          <FormControl>
-            <Input placeholder="Enter your Full Name" {...field} />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-  </div>
-);
-
-const Step3 = ({ control }) => (
-  <div>
-    <FormField
-      control={control}
-      name="university"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>University</FormLabel>
-          <FormControl>
-            <Input placeholder="Enter your University Name" {...field} />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-    <FormField
-      control={control}
-      name="college"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>College</FormLabel>
-          <FormControl>
-            <Input placeholder="Enter your College Name" {...field} />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
+const StepTwo = ({ control }) => (
+  <>
+    <h2 className="text-2xl font-semibold">Step 2: Academic Information</h2>
     <FormField
       control={control}
       name="branch"
@@ -192,98 +68,304 @@ const Step3 = ({ control }) => (
         </FormItem>
       )}
     />
+  </>
+);
+
+const StepThree = ({ control }) => (
+  <>
+    <h2 className="text-2xl font-semibold">Step 3: Core Interests</h2>
     <FormField
       control={control}
-      name="semester"
+      name="coreInterest"
       render={({ field }) => (
         <FormItem>
-          <FormLabel>Semester</FormLabel>
+          <FormLabel>Are you interested in the core of your branch?</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Select option" />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              <SelectItem value="yes">Yes</SelectItem>
+              <SelectItem value="no">No</SelectItem>
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  </>
+);
+
+const StepFour = ({ control }) => (
+  <>
+    <h2 className="text-2xl font-semibold">Step 4: Additional Interests</h2>
+    <FormField
+      control={control}
+      name="additionalInterests"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Any additional specific interests?</FormLabel>
           <FormControl>
-            <Input placeholder="Enter your Semester" {...field} />
+            <Input placeholder="Enter your Additional interests" {...field} />
           </FormControl>
           <FormMessage />
         </FormItem>
       )}
     />
-  </div>
+  </>
 );
 
-export default function Details() {
+const StepFive = ({ control }) => (
+  <>
+    <h2 className="text-2xl font-semibold">Step 5: Career Aspirations</h2>
+    <FormField
+      control={control}
+      name="careerGoals"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>What are your career goals as an engineer?</FormLabel>
+          <FormControl>
+            <Input placeholder="Enter your career goals" {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+    <FormField
+      control={control}
+      name="industryInterest"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Which industry sectors are you interested in?</FormLabel>
+          <FormControl>
+            <Input
+              placeholder="Enter industry sectors (e.g., technology, manufacturing)"
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+    <FormField
+      control={control}
+      name="environmentPreference"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>
+            Do you have a preference for working in a specific type of
+            environment?
+          </FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Select option" />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              <SelectItem value="corporate">Corporate</SelectItem>
+              <SelectItem value="startup">Startup</SelectItem>
+              <SelectItem value="research">Research Institution</SelectItem>
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+    <FormField
+      control={control}
+      name="skillsInterest"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>
+            Are there any particular skills or technologies you are passionate
+            about or wish to specialize in?
+          </FormLabel>
+          <FormControl>
+            <Input placeholder="Enter skills or technologies" {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  </>
+);
+
+const StepSix = ({ control }) => (
+  <>
+    <h2 className="text-2xl font-semibold">Step 6: Education and Skills</h2>
+    <FormField
+      control={control}
+      name="educationLevel"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>
+            What level of education have you achieved or are currently pursuing?
+          </FormLabel>
+          <FormControl>
+            <Input placeholder="Enter your education level" {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+    <FormField
+      control={control}
+      name="technicalSkills"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>
+            Are there any technical skills or certifications you have acquired
+            or are planning to acquire that are relevant to your career goals?
+          </FormLabel>
+          <FormControl>
+            <Input
+              placeholder="Enter technical skills or certifications"
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  </>
+);
+
+const StepSeven = ({ control }) => (
+  <>
+    <h2 className="text-2xl font-semibold">Step 7: Experience and Projects</h2>
+    <FormField
+      control={control}
+      name="internships"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>
+            Have you completed any internships or work experiences related to
+            your field of study?
+          </FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Select option" />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              <SelectItem value="yes">Yes</SelectItem>
+              <SelectItem value="no">No</SelectItem>
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+    <FormField
+      control={control}
+      name="projects"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>
+            Have you worked on any significant projects (academic or personal)
+            that relate to your career interests?
+          </FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Select option" />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              <SelectItem value="yes">Yes</SelectItem>
+              <SelectItem value="no">No</SelectItem>
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  </>
+);
+
+const Interests = () => {
   const [step, setStep] = useState(1);
   const form = useForm({
-    resolver: yupResolver(formSchema[step - 1]),
     mode: "onChange",
   });
 
-  const router = useRouter();
-
-  const onSubmit = async (data) => {
-    if (step < 3) {
+  const onSubmit = (data) => {
+    if (step < 7) {
       setStep(step + 1);
       return;
-    }
-    const response = await fetch("http://localhost:4000/profile/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-      credentials: "include",
-    });
-    const result = await response.json();
-    if (result.success) {
-      localStorage.setItem("user", JSON.stringify(result.user));
-      toast.success("Registered Successfully!", { autoClose: 1500 });
-      router.push("/dashboard");
-    } else {
-      toast.error(result.message, { autoClose: 1500 });
+    } else console.log(data); // Here you would typically send data to your backend for processing
+  };
+
+  const renderFormStep = () => {
+    switch (step) {
+      case 1:
+        return <StepOne control={form.control} />;
+      case 2:
+        return <StepTwo control={form.control} />;
+      case 3:
+        return <StepThree control={form.control} />;
+      case 4:
+        return <StepFour control={form.control} />;
+      case 5:
+        return <StepFive control={form.control} />;
+      case 6:
+        return <StepSix control={form.control} />;
+      case 7:
+        return <StepSeven control={form.control} />;
+      default:
+        return null;
     }
   };
 
   return (
-    <section className="min-h-screen flex items-center justify-center py-12 bg-background text-foreground transition duration-300 ease-in-out">
-      <div className="w-full max-w-lg bg-card shadow-lg rounded-lg p-10 dark:bg-gray-800 dark:text-white">
-        <div className="text-center mb-8">
-          <Link
-            href="/"
-            className="text-4xl font-extrabold text-blue-700 dark:text-blue-800"
-          >
-            Mr. Engineers
-          </Link>
-        </div>
-        <h1 className="text-3xl font-semibold text-center mb-6 dark:text-gray-100">
-          Fill additional details
-        </h1>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            {step === 1 && (
-              <Step1 control={form.control} errors={form.formState.errors} />
-            )}
-            {step === 2 && <Step2 control={form.control} />}
-            {step === 3 && <Step3 control={form.control} />}
-
-            <div
-              className={`flex w-full mt-4 ${
-                step == 1 ? "justify-end" : "justify-between"
-              }`}
+    <>
+      <section className="min-h-screen flex items-center justify-center py-12 bg-background text-foreground transition duration-300 ease-in-out">
+        <div className="w-full max-w-lg bg-card shadow-lg rounded-lg p-10 dark:bg-gray-800 dark:text-white">
+          <div className="text-center mb-8">
+            <Link
+              href="/"
+              className="text-4xl font-extrabold text-blue-700 dark:text-blue-800"
             >
-              {step > 1 && (
-                <Button
-                  type="button"
-                  className="w-max py-3 mt-4 bg-blue-600 text-white hover:bg-blue-700 transition duration-200 ease-in-out"
-                  onClick={() => setStep(step - 1)}
-                >
-                  Previous
-                </Button>
-              )}
-              <Button
-                type="submit"
-                className="w-max py-3 mt-4 bg-blue-600 text-white hover:bg-blue-700 transition duration-200 ease-in-out"
+              Mr. Engineers
+            </Link>
+          </div>
+          <h1 className="text-3xl font-semibold text-center mb-6 dark:text-gray-100">
+            Career Roadmap Prediction
+          </h1>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              {renderFormStep()}
+              <div
+                className={`flex w-full mt-4 ${
+                  step === 1 ? "justify-end" : "justify-between"
+                }`}
               >
-                {step < 3 ? "Next" : "Submit"}
-              </Button>
-            </div>
-          </form>
-        </Form>
-      </div>
-    </section>
+                {step > 1 && (
+                  <Button
+                    type="button"
+                    className="w-max py-3 mt-4 bg-blue-600 text-white hover:bg-blue-700 transition duration-200 ease-in-out"
+                    onClick={() => setStep(step - 1)}
+                  >
+                    Previous
+                  </Button>
+                )}
+                <Button
+                  type="submit"
+                  className="w-max py-3 mt-4 bg-blue-600 text-white hover:bg-blue-700 transition duration-200 ease-in-out"
+                >
+                  {step < 7 ? "Next" : "Submit"}
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </section>
+    </>
   );
-}
+};
+
+export default Interests;
