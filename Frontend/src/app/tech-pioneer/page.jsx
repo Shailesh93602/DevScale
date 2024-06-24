148b4a66644271221d243577cabb1206ef21ad4b
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-06-24 22:30:41 +0530

    INITIAL: Tech Assessment test

---
+++ Frontend/src/app/tech-pioneer/page.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const TechPioneer = () => (
+  <div>
+    <h1>Tech Pioneer</h1>
+    <ul>
+      <li>
+        <Link href="/tech-pioneer/coding-courses">Coding Courses</Link>
+      </li>
+      <li>
+        <Link href="/tech-pioneer/project-development">
+          Project Development
+        </Link>
+      </li>
+      <li>
+        <Link href="/tech-pioneer/ai-ml-courses">AI & ML Courses</Link>
+      </li>
+      <li>
+        <Link href="/tech-pioneer/virtual-internship">Virtual Internship</Link>
+      </li>
+    </ul>
+  </div>
+);
+
+export default TechPioneer;
