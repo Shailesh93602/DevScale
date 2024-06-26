d3e105330799fbab6304e1e1d5ed05a953954776
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-06-26 22:50:16 +0530

    Update logger.js

--- Backend/src/helpers/logger.js
+++ Backend/src/helpers/logger.js
@@ -1,14 +1,18 @@
 import winston from "winston";
-import fs from 'fs';
-import path from 'path';
-
-const logDir = 'logs';
-
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
 
 const { combine, json, timestamp, printf } = winston.format;
+
+// Create a "no-op" transport that does nothing
+class NoOpTransport extends winston.Transport {
+  log(info, callback) {
+    setImmediate(() => this.emit('logged', info));
+    callback();
+  }
+}
+
+const transports = [
+  new NoOpTransport() 
+];
 
 export const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
@@ -19,9 +23,5 @@ export const logger = winston.createLogger({
     json(),
     printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
   ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
-    new winston.transports.File({ filename: path.join(logDir, 'logInfo.log') }),
-  ],
+  transports
 });
