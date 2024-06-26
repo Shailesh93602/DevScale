a5aefd59f1cd70ef44b9cf195c96a2ee0c70b046
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-06-26 22:36:36 +0530

    Update logger.js

--- Backend/src/helpers/logger.js
+++ Backend/src/helpers/logger.js
@@ -1,4 +1,13 @@
 import winston from "winston";
+import fs from 'fs';
+import path from 'path';
+
+const logDir = 'logs';
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
 const { combine, json, timestamp, printf } = winston.format;
 
 export const logger = winston.createLogger({
@@ -8,14 +17,11 @@ export const logger = winston.createLogger({
       format: 'YYYY-MM-DD hh:mm:ss.SSS A',
     }),
     json(),
-    printf((info) => `[${info.timestamp}] ${info.level}: ${info}`)
+    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({
-      filename: 'logs/logInfo.log'
-    }),
+    new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.join(logDir, 'logInfo.log') }),
   ],
 });
-
