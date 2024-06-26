fc268139eb68503b9b5bbed75585cd9859a9fa48
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-06-26 22:59:26 +0530

    Update uploadMiddleware.js

--- Backend/src/middleware/uploadMiddleware.js
+++ Backend/src/middleware/uploadMiddleware.js
@@ -1,25 +1,18 @@
-import multer from "multer";
 import cloudinary from "../config/cloudinaryConfig.js";
 
-const upload = multer({ dest: "uploads/" });
-
-const uploadToCloudinary = (req, res, next) => {
-  upload.single("file")(req, res, async (err) => {
-    if (err) {
-      return res.status(400).json({ error: "Failed to upload file" });
+const uploadToCloudinary = async (req, res, next) => {
+  try {
+    if (!req.body.profilePicture) {
+      return res.status(400).json({ error: "No file uploaded" });
     }
 
-    try {
-      const result = await cloudinary.uploader.upload(req.body.profilePicture);
-      req.fileUrl = result.secure_url;
-      next();
-    } catch (error) {
-      console.error("Error uploading image:", error);
-      return res
-        .status(500)
-        .json({ error: "Failed to upload file to Cloudinary" });
-    }
-  });
+    const result = await cloudinary.uploader.upload(req.body.profilePicture);
+    req.fileUrl = result.secure_url;
+    next();
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    return res.status(500).json({ error: "Failed to upload file to Cloudinary" });
+  }
 };
 
 export default uploadToCloudinary;
