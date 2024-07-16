0e8158e3a9b82cad07ac0987358570bbe27a4a44
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-07-16 08:27:21 +0530

    ADDED: available topics listing, create resource page with resource id with respected routes and controllers

--- Backend/src/routes/resourceRoutes.js
+++ Backend/src/routes/resourceRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createResource,
   getResource,
+  getResourceDetails,
   getResources,
 } from "../controllers/resourceController.js";
 
@@ -9,6 +10,8 @@ const router = express.Router();
 
 router.get("/", getResources);
 router.get("/:id", getResource);
+router.get("/list", getResourceList);
+router.get("/details/:id", getResourceDetails);
 router.post("/create", createResource);
 
 export default router;
