66e625b71ac1b7cbc896615d999861c0d96ee79e
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-07-23 07:54:34 +0530

    FIX: resolved bugs in cors setup

--- Backend/index.js
+++ Backend/index.js
@@ -24,16 +24,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: [
-      "http://localhost:3000",
-      "https://localhost:3001",
-      "https://mrengineers.vercel.app",
-    ],
-  })
-);
+app.use(cors());
 app.use(routes);
 applyPassportStrategy();
 
