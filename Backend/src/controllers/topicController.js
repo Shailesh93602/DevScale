073706b5874fb509913abbe759d454f4de417529
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-08-09 07:53:36 +0530

    ADDED: logic to get unpublished topics and ignored approved topics

--- Backend/src/controllers/topicController.js
+++ Backend/src/controllers/topicController.js
@@ -1,22 +1,20 @@
 import db from "../../db/models/index.js";
-import { Op } from "sequelize";
+import { Op, Sequelize } from "sequelize";
 import { logger } from "../helpers/logger.js";
 
 export const getUnpublishedTopics = async (req, res) => {
   try {
     const topics = await db.Topic.findAll({
-      include: [
-        {
-          model: db.Article,
-          attributes: ["id", "status"],
-          required: false,
-          where: {
-            status: { [Op.ne]: "approved" },
-          },
-        },
-      ],
       where: {
-        "$Articles.id$": null,
+        id: {
+          [Op.in]: Sequelize.literal(`(
+            SELECT DISTINCT \`Topics\`.\`id\`
+            FROM \`Topics\`
+            LEFT JOIN \`Articles\` ON \`Articles\`.\`topicId\` = \`Topics\`.\`id\`
+            WHERE \`Articles\`.\`status\` IS NULL
+            OR \`Articles\`.\`status\` != 'approved'
+          )`),
+        },
       },
     });
 
