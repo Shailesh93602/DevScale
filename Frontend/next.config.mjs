9fc799b6096a7cc469dc8619737e33a9d95a6a15
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-09-05 15:38:04 +0530

    vaibhav

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -4,8 +4,22 @@ const nextConfig = {
     autoPrerender: false,
   },
   images: {
-    domains: ["aceternity.com", "images.unsplash.com"],
+    domains: [
+      "assets.aceternity.com",
+      "images.unsplash.com",
+      "www.python.org",
+      "example.com",
+      "anotherdomain.com",
+      "www.python.org",
+      "v17.angular.io",
+      "developer.mozilla.org",
+      "www.php.net",
+      "www.java.com",
+      "flutter.dev",
+    ],
   },
 };
 
 export default nextConfig;
+
+// next.config.js
