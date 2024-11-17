f8a91aad53d828fd6e19ccb6a7ebe23dafbaacc3
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-11-17 09:57:21 +0530

    Added controller and routes for getting, creating and deleting user roadmaps

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import {
+  deleteUserRoadmap,
   getProfile,
   getUserProgress,
+  getUserRoadmap,
   insertProfile,
+  insertUserRoadmap,
   updateProfile,
 } from "../controllers/userControllers.js";
 import { userInsertionValidator } from "../validators/userValidators.js";
@@ -14,5 +17,8 @@ router.get("/", getProfile);
 router.post("/register", userInsertionValidator, insertProfile);
 router.put("/update", uploadToCloudinary, updateProfile);
 router.get("/progress", getUserProgress);
+router.get("/roadmap", getUserRoadmap);
+router.post("/roadmap", insertUserRoadmap);
+router.delete("/roadmap/:id", deleteUserRoadmap);
 
 export default router;
